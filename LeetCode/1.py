04639d80b38e1783db191851d8863e96d7ee33c1
郝永彬 <845415849@qq.com>
2018-04-20 21:00:14 +0800

    No.1

--- LeetCode/1.py
+++ LeetCode/1.py
@@ -1,1 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+        for i in range(length):
+            for j in range(i + 1,length):
+                if target == nums[i] + nums[j]:
+                    return [i,j]
 
+if __name__ == '__main__':
+    Solution().twoSum(nums = [2, 7, 11, 15], target = 9)
